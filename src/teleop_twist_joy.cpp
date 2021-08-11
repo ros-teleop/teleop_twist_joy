5290f47fd594667f10c8438e990b5c1ebbddef47
Fernando A <fernando.aragon@tgfoe.org>
2021-08-11 10:12:58 -0600

    Adding manual, auto and xci_control buttons and publications

--- src/teleop_twist_joy.cpp
+++ src/teleop_twist_joy.cpp
@@ -23,6 +23,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 */
 
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 #include "teleop_twist_joy/teleop_twist_joy.h"
@@ -42,12 +44,19 @@ struct TeleopTwistJoy::Impl
 {
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   void sendCmdVelMsg(const sensor_msgs::Joy::ConstPtr& joy_msg, const std::string& which_map);
+  void sendModeCmdMsg(const std::string& mode);
+  void sendXciControlEnable(const sensor_msgs::Joy::ConstPtr& joy_msg);
 
   ros::Subscriber joy_sub;
   ros::Publisher cmd_vel_pub;
+  ros::Publisher mode_cmd_pub;
+  ros::Publisher xci_control_pub;
 
   int enable_button;
   int enable_turbo_button;
+  int manual_button;
+  int auto_button;
+  int xci_control_button;
 
   std::map<std::string, int> axis_linear_map;
   std::map< std::string, std::map<std::string, double> > scale_linear_map;
@@ -68,10 +77,15 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
   pimpl_ = new Impl;
 
   pimpl_->cmd_vel_pub = nh->advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
+  pimpl_->mode_cmd_pub = nh->advertise<std_msgs::String>("send_command", 1, true);
+  pimpl_->xci_control_pub = nh->advertise<std_msgs::Bool>("xci_control_enable", 1, true);
   pimpl_->joy_sub = nh->subscribe<sensor_msgs::Joy>("joy", 1, &TeleopTwistJoy::Impl::joyCallback, pimpl_);
 
   nh_param->param<int>("enable_button", pimpl_->enable_button, 0);
   nh_param->param<int>("enable_turbo_button", pimpl_->enable_turbo_button, -1);
+  nh_param->param<int>("manual_button", pimpl_->manual_button, -1);
+  nh_param->param<int>("auto_button", pimpl_->auto_button, -1);
+  nh_param->param<int>("xci_control_button", pimpl_->xci_control_button, -1);
 
   if (nh_param->getParam("axis_linear", pimpl_->axis_linear_map))
   {
@@ -110,9 +124,16 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
         pimpl_->scale_angular_map["turbo"]["yaw"], pimpl_->scale_angular_map["normal"]["yaw"]);
   }
 
+  // output current setup to screen
   ROS_INFO_NAMED("TeleopTwistJoy", "Teleop enable button %i.", pimpl_->enable_button);
   ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy",
       "Turbo on button %i.", pimpl_->enable_turbo_button);
+  ROS_INFO_COND_NAMED(pimpl_->manual_button >= 0, "TeleopTwistJoy",
+      "Manual on button %i.", pimpl_->manual_button);
+  ROS_INFO_COND_NAMED(pimpl_->auto_button >= 0, "TeleopTwistJoy",
+      "Autonomous on button %i.", pimpl_->auto_button);
+  ROS_INFO_COND_NAMED(pimpl_->xci_control_button >= 0, "TeleopTwistJoy",
+      "XCI control on button %i.", pimpl_->xci_control_button);
 
   for (std::map<std::string, int>::iterator it = pimpl_->axis_linear_map.begin();
       it != pimpl_->axis_linear_map.end(); ++it)
@@ -165,8 +186,34 @@ void TeleopTwistJoy::Impl::sendCmdVelMsg(const sensor_msgs::Joy::ConstPtr& joy_m
   sent_disable_msg = false;
 }
 
+// teleop_twist_joy will send piloting mode messages to Project 11 command_bridge_sender
+// default rostopic: /send_command
+// data: "piloting_mode autonomous"  or  "piloting_mode manual"
+void TeleopTwistJoy::Impl::sendModeCmdMsg(const std::string& mode)
+{
+  std_msgs::String cmd_mode_msg;
+  cmd_mode_msg.data = "piloting mode " + mode;
+  mode_cmd_pub.publish(cmd_mode_msg);
+}
+
+// teleop_twist_jow will send xci control enable commands to blue_arrow interface
+// default rostopic: /xci_control_enable
+// data: bool
+void TeleopTwistJoy::Impl::sendXciControlEnable(const sensor_msgs::Joy::ConstPtr& joy_msg)
+{
+  std_msgs::Bool xci_control;
+  xci_control.data  = (bool)joy_msg->buttons[xci_control_button];
+  xci_control_pub.publish(xci_control);
+}
+
 void TeleopTwistJoy::Impl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
 {
+  // Always publish xci enable
+  if (joy_msg->buttons.size() > xci_control_button)
+  {
+    sendXciControlEnable(joy_msg);
+  }
+
   if (enable_turbo_button >= 0 &&
       joy_msg->buttons.size() > enable_turbo_button &&
       joy_msg->buttons[enable_turbo_button])
@@ -177,6 +224,16 @@ void TeleopTwistJoy::Impl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg
            joy_msg->buttons[enable_button])
   {
     sendCmdVelMsg(joy_msg, "normal");
+  }
+  else if (joy_msg->buttons.size() > manual_button &&
+           joy_msg->buttons[manual_button])
+  {
+    sendModeCmdMsg("manual");
+  }
+  else if (joy_msg->buttons.size() > auto_button &&
+           joy_msg->buttons[auto_button])
+  {
+    sendModeCmdMsg("autonomous");
   }
   else
   {
